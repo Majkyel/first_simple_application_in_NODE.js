ae5250f1aac207c1b4ecbf68cdd712d74e6d44f2
Michał Kuliś <michalkulis1993@gmail.com>
2018-07-06 01:05:35 +0200

    Update program.js

--- program.js
+++ program.js
@@ -2,21 +2,20 @@ process.stdin.setEncoding('utf-8');
 
 process.stdin.on('readable', function () {
     var input = process.stdin.read();
-    if (input !== null) {
-        var instruction = input.toString().trim();
-        switch (instruction) {
-            case '/exit':
-                process.stdout.write('Quiting app!\n');
-                process.exit();
+    if (!input) {return;}
+    var instruction = input.toString().trim();
+    switch (instruction) {
+        case '/exit':
+            process.stdout.write('Quiting app!\n');
+            process.exit();
             break;
-            case '/versions':
-                console.log(process.versions);
+        case '/versions':
+            console.log(process.versions);
             break;
-            case '/system_language':
-                console.log(process.env);
+        case '/system_language':
+            console.log(process.env);
             break;
-            default:
-             process.stderr.write('Wrong instruction!\n');
-        }
+        default:
+            process.stderr.write('Wrong instruction!\n');
     }
 })
